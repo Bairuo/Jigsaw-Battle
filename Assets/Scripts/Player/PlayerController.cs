7518a624057580b75533d1de868ec308940ead8d
Fang Yilin <935474206@qq.com>
2017-07-07 17:42:38 +0800

    add the first controll module

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.gameObject.GetComponentInChildren<TransformController>().TransformChanger(collision.gameObject);
+        Destroy(this.gameObject);
+    }
+}
