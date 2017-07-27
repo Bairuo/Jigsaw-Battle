45a7659376ed2bf36066a2be522d562a2e1c0087
Fang Yilin <935474206@qq.com>
2017-07-27 16:06:27 +0800

    fixed the appearence of the circle

---
+++ Assets/Scripts/UI/BlockController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BlockController : MonoBehaviour {
+
+    public Sprite obj1;
+    public Sprite obj2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
