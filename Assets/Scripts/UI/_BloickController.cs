f86f400101c29f58d2f98fd597bab3fef7738965
Fang Yilin <935474206@qq.com>
2017-07-28 15:56:53 +0800

    fix the start scene

---
+++ Assets/Scripts/UI/_BloickController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _BloickController : MonoBehaviour {
+
+    public float speed;
+    private IEnumerator coroutine;
+    // Use this for initialization
+    void Start () {
+        coroutine = WaitAndDestroy(1.0f);
+        StartCoroutine(coroutine);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator WaitAndDestroy(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(this.gameObject);
+    }
+}
