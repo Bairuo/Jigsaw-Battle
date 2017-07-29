9477eb06f7e29d5994da939a5f2b78f7884e7fb2
Zhengru Jiang <jzr98@qq.com>
2017-07-29 13:57:14 +0700

    Add Network connect and change to game scene

---
+++ Assets/Scripts/Network/NetUnitData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class NetUnitData{
+    //对于玩家来说id是玩家在房间内的id
+    //对于其它物体来说id是创建端的InstanceID
+    string id;
+
+    public NetUnitData(string _id, GameObject obj)
+    {
+        id = _id;
+        float x = obj.gameObject.transform.position.x;
+        float y = obj.gameObject.transform.position.y;
+        float z = obj.gameObject.transform.position.z;
+
+        if (obj.tag != "Player")
+        {
+            if (Client.instance.playerid != "0")
+            {
+                Rigidbody2D r = obj.GetComponent<Rigidbody2D>();
+                r.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+
+        }
+        else
+        {
+            if (id != Client.instance.playerid)
+            {
+                Rigidbody2D r = obj.GetComponent<Rigidbody2D>();
+                r.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+
+    }
+
+    public static ProtocolBytes GetUnitData(string protoName, string id, Vector3 pos)
+    {
+        ProtocolBytes proto = new ProtocolBytes();
+        proto.AddString(protoName);
+        proto.AddString(id);
+
+        proto.AddFloat(pos.x);
+        proto.AddFloat(pos.y);
+        proto.AddFloat(pos.z);
+
+        return proto;
+    }
+
+}
--- Assets/Scripts/UI/SceneManager.cs
+++ Assets/Scripts/UI/SceneManager.cs
@@ -3,9 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour {
+    public static SceneManager instance;
+
+    public SceneManager(){
+        instance = this;
+    }
+
+    void Update()
+    {
+        Client.instance.Update();
+    }
+
+    void Start()
+    {
+        if (ServerNet.instance == null) new ServerNet();
+        if (Client.instance == null) new Client();
+    }
+
     public void OnStartClick()
     {
-        Debug.Log("Click!");
+        Client.instance.Connect();
+    }
+
+    public void StartScene()
+    {
         Application.LoadLevel("dk-test");
     }
 }
